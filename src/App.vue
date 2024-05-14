23b1daeb09953465fc695f22fc1cbf5d9c36e012
ZtionJam <work@ztion.cn>
2024-05-14 16:15:09 +0800

    tow commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup>
 import  A from "./components/A.vue";
+// 禁用触摸板拖动的函数
+function disableDrag(e) {
+  e.preventDefault();
+}
 
+// 页面加载时绑定事件
+window.onload = function() {
+  // 添加事件监听到文档，用于阻止拖动事件
+  document.addEventListener('touchmove', disableDrag, { passive: false });
+};
 </script>
 
 <template>
